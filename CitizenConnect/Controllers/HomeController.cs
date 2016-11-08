b84bb517f1073d988d6f21e52c57e0ef4d21e8b6
Stephen2Page <stephen2page@gmail.com>
2016-11-08 11:32:13 -0500

    Revert "Attempting to Hide APIkey in model"
    This reverts commit 9adf8c9326e39592ccfd1fb420d429dc4f7b2c1c.

--- CitizenConnect/Controllers/HomeController.cs
+++ CitizenConnect/Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using CitizenConnect.Models;
 
 namespace CitizenConnect.Controllers
 {
@@ -32,11 +31,6 @@ namespace CitizenConnect.Controllers
             ViewBag.Message = "Address Lookup Page.";
 
             return View();
-        }
-        public JsonResult GetAddress()
-        {
-            WardInfo address = new WardInfo();
-            return Json(address.getAddress(), JsonRequestBehavior.AllowGet);
         }
     }
 }
--- CitizenConnect/Models/WardInfo.cs
+++ CitizenConnect/Models/WardInfo.cs
@@ -2,21 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Net;
-using System.Web.Script.Serialization;
 
 namespace CitizenConnect.Models
 {
     public class WardInfo
     {
-        public Object getAddress()
-        {
-            string url = "https://maps.googleapis.com/maps/api/js?key=AIzaSyA-UEiyhrkeVKo2UfwX3WNKqToGvgL9yFc&libraries=places";
-            var client = new WebClient();
-            var content = client.DownloadString(url);
-            var serializer = new JavaScriptSerializer();
-            var jsonContent = serializer.Deserialize<Object>(content);
-            return jsonContent;
-        }
+        //public Object getWardInfo()
+        //{
+        //    string url = "https://maps.googleapis.com/maps/api/js?key=AIzaSyA-UEiyhrkeVKo2UfwX3WNKqToGvgL9yFc&libraries=places";
+
+        //}
     }
 }
