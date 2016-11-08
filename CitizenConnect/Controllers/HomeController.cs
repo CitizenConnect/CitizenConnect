9adf8c9326e39592ccfd1fb420d429dc4f7b2c1c
Stephen2Page <stephen2page@gmail.com>
2016-11-08 11:31:21 -0500

    Attempting to Hide APIkey in model

--- CitizenConnect/Controllers/HomeController.cs
+++ CitizenConnect/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CitizenConnect.Models;
 
 namespace CitizenConnect.Controllers
 {
@@ -31,6 +32,11 @@ namespace CitizenConnect.Controllers
             ViewBag.Message = "Address Lookup Page.";
 
             return View();
+        }
+        public JsonResult GetAddress()
+        {
+            WardInfo address = new WardInfo();
+            return Json(address.getAddress(), JsonRequestBehavior.AllowGet);
         }
     }
 }
